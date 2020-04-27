7bc282c56e3dc76c465f38a7cee94fbb2da3ad60
ripplefox <lr@ripplefox.com>
2020-04-27 15:16:15 +0800

    add default gateway for testing net

--- js/service/Gateways.js
+++ js/service/Gateways.js
@@ -1,7 +1,6 @@
 /* global _, myApp */
 
 myApp.factory('Gateways', ['$rootScope', function($rootScope) {
-  
     let _gateways = {
         "ripplefox.com" : {
           name : 'ripplefox.com',
@@ -47,12 +46,21 @@ myApp.factory('Gateways', ['$rootScope', function($rootScope) {
             {code : 'BTC', issuer : 'rchGBxcD1A1C2tdxF6papQYZ8kjRKMYcL', list: true}
           ],
           logo : "img/gateway/gatehub.png"
-        },
-        "unkown" : {
-          name : '',
-          website : '',
-          assets: [],
-          logo : 'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAMAAABHPGVmAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAIBUExURf///344Ffr4+IE7Gfz7+3YrBnQoAnQnAJ5pT4RAH3csB9vIv3szD4E8GoA5F+bZ0385FnYqBYtLLHguCYI+HKJvVnQnAXctCP39/fDo5Y9RM3UpBKZ1XYA6GO/m46BrUqRxWZ9qULqVg9S9snw0EY9SNL+ci5JWOa+DbuXY0pBTNcmsnpxmTLiSf6h5Yp9rUfz8/IdEJPby8dfCuKV0XIpKKufa1f7+/vHq56BsU8SkldrGvX43FOLSy7aOe+3k4LGGcYVCIerf2vDp5qh4Yd7NxbuWhK6CbaJwV97MxPTu7L2ZiMGgkJtlSreQfXoxDZNXOufa1MiqnO7l4XUpA6d3YL6aicWllq2Ba9zJwffz8ujb1vr398Oiko5QMdG5rfv6+qx+aMCdjend2LuXhaVzW/n29XowDLGHcqRyWnkvC/Ps6pNYO/bx8IZDInguCta/tdvHvr+djHsyDrOJdZVbP/Lr6e/n5N3Lw+TW0IZEI/n29ppjSK2AasaomZhfQ9O7sIM/HppiR+HQybqUgvXw78qtn8Wml6FtVLKHc7yYhqd2X5JVOJ5oTptkSX02E7mSgKZ2Xujc15BUNodFJfv5+c+1qZRZPHw0EIhGJu7l4rKIdMywo8epm+vh3fHr6NW+s6l6Y+rg28uvormTgeTVz301EopKK9C2qpFVN5RaPc2k5EgAAAKESURBVGje7dj3UxpBFAfwBwc8QUBABaJAIIIkBBKIvSYaY+waW0zvvffee++a3vtfmd1DQo4w42WGN+PE/f6ws/tu9j5zt3twcwAiIiIiIiIiMzwn1tU5gnqTzVFZv4eION5Wir9j99RSGJ02VMRckXvjym7MiKkh58gxfl7DtjKfVuu7GJAV4/ZcI6vPOVHKT4022LlyPvc37GSkID3YypGzxLt5Ed9pl6ifmSqGuKiREoZoiI2aRoasJ0ZW8YVfQmu493LkFC2ykxu7SImNm7hh6aI0qvnOQtsaysfwqJ4bp6vpiDOHNfKPY+U+wuvwysT+g6Rrzokd7WuBHKkgftBlJF8g0woRmX45tLlnGbXRzHdXOTFi4UgVMXJA/kskRsIcuUyMNJXoj1zYIva/iIiISOo9KE8g/zmSPJ3cYl7BbLNmri41voVXFcWhEcnufbSY9SQHwFt8xXoho3Li1IixsKjYiPMnx814W1GcZTX4H/oN1qUAUVMcVmIC4Jneo5w4NaKfo4N5uCA5/px820oXx/A7G4/jcoAYfoSABVdAEw4qJ6q4XazR4kK5O4o96eNyMZF8MZJeAJTh+7h50NXOLqdVOVEdMtkNov1LxnHTT7kw4WJ3Dss7DV3RELRJGef4JwRHvn7TZiA/5AlP+Ic16UMsAm+cPkeHKsR5PysScX9KfndKFxM2N/9apHnK2uhAuA/eGUaH61UhwRYd1Nb9hbCm19mgKI7ha9a7zhceYv3D7FU/3MjWXg3SiwP3CkPZkJf9Rdf+LLIt3NFXLG9htvLP4wDd+KBGFdJ619TSrcuGwA30K4pDd7ylVg9/GNnK32TtYwyAKkRERERERGTG5xfV+1fLS6+G/gAAAABJRU5ErkJggg==',
+        }
+    };
+    
+    let _testingnet = {
+        "xrptoolkit.com" : {
+          name : 'xrptoolkit.com',
+          website : 'https://xrptoolkit.com',
+          service : [],
+          assets : [
+            {code : 'USD', issuer : 'rD9W7ULveavz8qBGM1R5jMgK2QKsEDPQVi', list: true, name: "USD (Testing)"},
+            {code : 'BTC', issuer : 'rD9W7ULveavz8qBGM1R5jMgK2QKsEDPQVi', list: true, name: "BTC (Testing)"},
+            {code : 'ETH', issuer : 'rD9W7ULveavz8qBGM1R5jMgK2QKsEDPQVi', name: "ETH (Testing)"},
+            {code : 'EUR', issuer : 'rD9W7ULveavz8qBGM1R5jMgK2QKsEDPQVi', name: "EUR (Testing)"},
+          ],
+          logo : "img/gateway/xrptoolkit.png"
         }
     };
     
@@ -78,6 +86,14 @@ myApp.factory('Gateways', ['$rootScope', function($rootScope) {
         }
       });
     }
+    //add testing net asset to asset2gateway
+    _testingnet["xrptoolkit.com"].assets.forEach(asset => {
+      _asset2gateway[asset.issuer] = {
+          name : _testingnet["xrptoolkit.com"].name,
+          website : _testingnet["xrptoolkit.com"].website,
+          logo : _testingnet["xrptoolkit.com"].logo
+      }
+    });
     
     return {
       getGateway(code, issuer) {
@@ -86,11 +102,15 @@ myApp.factory('Gateways', ['$rootScope', function($rootScope) {
             logo : $rootScope.currentNetwork.coin.logo
           }
         }
-        return  _asset2gateway[key(code, issuer)] || _asset2gateway[issuer] ||_gateways["unkown"];
+        return _asset2gateway[key(code, issuer)] || _asset2gateway[issuer] || {logo : 'img/unknown.png'};
       },
       
       get gateways() {
-        return _gateways;
+        if ($rootScope.currentNetwork.networkType == 'xrpTest') {
+          return _testingnet;
+        } else {
+          return _gateways;
+        }
       },
       
       get defaultTradeAssets() {
